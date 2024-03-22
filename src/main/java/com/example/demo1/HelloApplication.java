d8df1960fe68a3966c2d220400bdf78726fedb81
karlosian <kjmiles360@gmail.com>
2024-03-22 14:04:05 -0400

    Added buttons and gridpane

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -11,10 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800,450 );
+        Scene scene = new Scene(fxmlLoader.load(), 720,1280 );
         stage.setTitle("Wordle");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -2,13 +2,40 @@ package com.example.demo1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable{
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private GridPane textFieldGrid;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Loop through each cell and add TextField
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 5; col++) {
+                TextField textField = new TextField();
+                textField.setAlignment(javafx.geometry.Pos.CENTER);
+                textField.setPrefHeight(50.0);
+                textField.setPrefWidth(50.0);
+                textField.setMaxWidth(50.0);
+                textField.setMinHeight(50.0);
+                textField.getStyleClass().add("GridClass"); // Apply CSS class
+                textFieldGrid.add(textField, col, row);
+
+                GridPane.setMargin(textField, new javafx.geometry.Insets(35));
+            }
+        }
     }
 }
