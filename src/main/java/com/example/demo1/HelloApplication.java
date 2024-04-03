38dd7399017eae7b320d177c931c394d862110f7
karlosian <kjmiles360@gmail.com>
2024-04-03 15:13:55 -0400

    Made input of words work properly, limiting 1 word before enter. Made backspace work. Made enter create a string for the word written.

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720,900f );
+        Scene scene = new Scene(fxmlLoader.load(), 720,900 );
         stage.setTitle("Wordle");
         scene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
         stage.setScene(scene);
--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -9,6 +9,8 @@
     import javafx.scene.control.Button;
     import javafx.scene.layout.GridPane;
 
+
+    import java.lang.StringBuilder;
     import java.net.URL;
     import java.util.ResourceBundle;
 
@@ -18,6 +20,12 @@
         private GridPane textFieldGrid;
 
         private static int placeTracker = 0;
+
+        private static int rowIndex = 0;
+
+        private static boolean rowFull = false;
+
+        private String finalString = " ";
 
         public void initialize(URL url, ResourceBundle resourceBundle) {
             // Loop through each cell and add TextField
@@ -42,10 +50,55 @@
             }
         }
         public void addLetter(ActionEvent event) {
+
             Button clickedButton = (Button) event.getSource();
             String buttonId = clickedButton.getId();
-            placeTracker++;
-            TextField tf = (TextField) textFieldGrid.lookup(("#" + placeTracker));
-            tf.setText(buttonId);
+
+            TextField lastOfRow = (TextField) textFieldGrid.lookup("#" + ((1+rowIndex)*5));
+            char c = lastOfRow.getCharacters().charAt(0);
+            if(c != ' ') {
+                rowFull = true;
+            }
+            else rowFull = false;
+
+            if(!rowFull){
+                placeTracker++;
+                TextField tf = (TextField) textFieldGrid.lookup(("#" + placeTracker));
+                tf.setText(buttonId);}
+        }
+        //logic for when backspace key is pressed
+        public void backSpace(ActionEvent event){
+            //check if backspace can even be pressed
+            if(placeTracker != rowIndex*5) {
+                if (placeTracker != 0) {
+                    TextField tf = (TextField) textFieldGrid.lookup(("#" + placeTracker));
+                    tf.setText(" ");
+                }
+                if (placeTracker > 0) {
+                    placeTracker--;
+                }
+            }
+        }
+
+        //logic for when enter key is pressed
+        public void enterKey(ActionEvent event){
+            TextField lastOfRow = (TextField) textFieldGrid.lookup("#" + ((1+rowIndex)*5));
+            char c = lastOfRow.getCharacters().charAt(0);
+            //make sure row is full
+            if(c != ' '){
+                rowIndex++;
+                // -- add code to look at word vs wordle in the future --
+                rowFull = false;
+                //declare all 5 text fields of the row
+                TextField fir = (TextField) textFieldGrid.lookup("#" + (placeTracker - 4));
+                TextField sec = (TextField) textFieldGrid.lookup("#" + (placeTracker - 3));
+                TextField thr = (TextField) textFieldGrid.lookup("#" + (placeTracker - 2));
+                TextField frt = (TextField) textFieldGrid.lookup("#" + (placeTracker - 1));
+
+                StringBuilder sb = new StringBuilder().append(fir.getCharacters().charAt(0)).append(sec.getCharacters().charAt(0)).append(thr.getCharacters().charAt(0)).append(frt.getCharacters().charAt(0)).append(c);
+                finalString = sb.toString();
+
+                System.out.println("FinalString: " +  finalString);
+            }
         }
     }
